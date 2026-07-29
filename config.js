822d72c9a50648681fca87e8d474fa2beb297885
0905dhruvshah <0905.dhruvshah@gmail.com>
2026-07-29 19:09:02 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,1 @@
-const API_URL="https://script.google.com/macros/s/AKfycbzv7mgw7dUOAMD66zud6LxGkxMfoCI5G5HtfkM8NmB5AnCnAkENJ-O3-Hlozn7aQQSG/exec";
+const API_URL = "https://script.google.com/macros/s/AKfycbzv7mgw7dUOAMD66zud6LxGkxMfoCI5G5HtfkM8NmB5AnCnAkENJ-O3-Hlozn7aQQSG/exec";
